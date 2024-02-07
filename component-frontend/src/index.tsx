db2ea53848cf88b80ff166305a338731d0cf2f79
Suryank <suryanksingh_2@outlook.com>
2024-02-07 23:52:59 +0530

    index.tsx

--- component-frontend/src/index.tsx
+++ component-frontend/src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Scenario from './Scenario';
 import AdjValue from './AdjValue';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
@@ -13,10 +12,8 @@ root.render(
         <Route path='/AdjValue' element={<AdjValue/>}/>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
